a9c4039b01028ce66b67c5221a3ab141670c0bc3
AS5410 <aleksandrskripnikov568@gmail.com>
2026-03-02 22:41:53 +0000

    Add window realization

--- include/Platform/IWindow.hpp
+++ include/Platform/IWindow.hpp
@@ -4,6 +4,9 @@
 #include <Base/IModule.hpp>
 #include <Base/ResultOrError.hpp>
 
+#define AS_ENGINE_IWINDOW_SURFACETYPE_OPENGL 0x0000000000000002ULL
+#define AS_ENGINE_IWINDOW_SURFACETYPE_VULKAN 0x0000000010000000ULL
+
 namespace AS::Engine {
 struct WindowSize {
   unsigned int x;
@@ -11,10 +14,20 @@ struct WindowSize {
 };
 class ENGINE_EXPORT IWindow {
  public:
+  virtual void Initialize() = 0;
+
+  virtual void SetSurfaceType(unsigned long long surfaceType) = 0;
   virtual void SetSize(const WindowSize& size) = 0;
+  virtual void SetFullscreen(bool isItTrue) = 0;
+  virtual void SetTitle(const char* title) = 0;
 
   virtual WindowSize GetSize() const = 0;
   virtual ResultOrError<void*> GetSurface() = 0;
+  virtual unsigned long long GetSurfaceType() = 0;
+  virtual bool GetFullscreen() = 0;
+  virtual const char* GetTitle() = 0;
+
+  virtual void Update() = 0;
 
   virtual void Destroy() = 0;
 
---
+++ src/Platform/SDL3/Window.cpp
@@ -0,0 +1,60 @@
+#include <Platform/SDL3/Window.hpp>
+
+using namespace AS::Engine;
+
+Window::Window() { SDL_Init(SDL_INIT_VIDEO); }
+
+void Window::Initialize() {
+  window = SDL_CreateWindow(Title, Size.x, Size.y, Flag | SDL_WINDOW_RESIZABLE);
+}
+
+void Window::SetSize(const WindowSize& size) {
+  if (!window) return;
+  Size = size;
+  SDL_SetWindowSize(window, size.x, size.y);
+}
+void Window::SetFullscreen(bool isItTrue) {
+  Fullscreen = isItTrue;
+  if (window) SDL_SetWindowFullscreen(window, isItTrue);
+}
+void Window::SetTitle(const char* title) {
+  if (window) SDL_SetWindowTitle(window, title);
+  Title = title;
+}
+
+ResultOrError<void*> Window::GetSurface() {
+  if (!window) return ResultOrError<void*>(0, "Window = 0", true);
+  if (Flag != AS_ENGINE_IWINDOW_SURFACETYPE_VULKAN)
+    return ResultOrError<void*>(0,
+                                "You trying get surface for OPENGL??? Are you "
+                                "serious??? This is only vulkan function!!!",
+                                true);
+  return SDL_GetWindowSurface(window);
+}
+
+void Window::Update() {
+  if (!window) return;
+  SDL_Event e;
+  while (SDL_PollEvent(&e)) {
+    switch (e.type) {
+      case SDL_EVENT_QUIT:
+        Destroy();
+        break;
+      case SDL_EVENT_WINDOW_RESIZED:
+        int x, y;
+        SDL_GetWindowSize(window, &x, &y);
+        Size.x = x;
+        Size.y = y;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+void Window::Destroy() { SDL_DestroyWindow(window); }
+
+Window::~Window() {
+  if (window) Destroy();
+  if (InitializedSDL) SDL_Quit();
+}
