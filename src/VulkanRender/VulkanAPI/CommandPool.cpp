f3206cc06b6eacf2752194ed0069f6d390eb4275
AS5410 <aleksandrskripnikov568@gmail.com>
2026-03-04 11:55:15 +0000

    Render: Vulkan: Refactor CommandPool class

--- src/VulkanRender/VulkanAPI/CommandPool.cpp
+++ src/VulkanRender/VulkanAPI/CommandPool.cpp
@@ -1,33 +1,16 @@
 #include "CommandPool.hpp"
 
-CommandPool::CommandPool( ) { }
+CommandPool::CommandPool(Device& device, unsigned int queueFamily)
+    : Dev(device), QueueFamily(queueFamily) {
+  VkCommandPoolCreateInfo cpcInfo{
+      .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+      .pNext = 0,
+      .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+      .queueFamilyIndex = queueFamily};
+  vkCreateCommandPool(Dev.GetDevice(), &cpcInfo, 0, &Pool);
+}
 
-CommandPool::CommandPool( Device *device, unsigned int queueFamily ) : QueueFamily( queueFamily )
-{
-	Dev = device;
-	vk::CommandPoolCreateInfo cpcInfo{
-		.flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
-		.queueFamilyIndex = queueFamily
-	};
-	Pool = new vk::raii::CommandPool( device->GetDevice( ), cpcInfo );
-}
-CommandPool::CommandPool( vk::raii::CommandPool *pool ) : Pool( pool )
-{ }
-
-vk::raii::CommandPool &CommandPool::operator *( )
-{
-	return *Pool;
-}
-Device *CommandPool::GetDevice( )
-{
-	return Dev;
-}
-void CommandPool::Release( )
-{
-	Pool->release( );
-	Pool = 0;
-}
-unsigned int CommandPool::GetQueueFamily( )
-{
-	return QueueFamily;
+void CommandPool::Release() {
+  vkDestroyCommandPool(Dev.GetDevice(), Pool, 0);
+  Pool = 0;
 }
--- src/VulkanRender/VulkanAPI/CommandPool.hpp
+++ src/VulkanRender/VulkanAPI/CommandPool.hpp
@@ -1,26 +1,26 @@
 #ifndef REF_COMMAND_POOL_HPP
 #define REF_COMMAND_POOL_HPP
-#include "Vulkan.hpp"
 #include "Device.hpp"
+#include "Vulkan.hpp"
 
-class CommandPool final
-{
-public:
-	CommandPool( );
-	CommandPool( Device *device, unsigned int queueFamily );
-	CommandPool( vk::raii::CommandPool *pool );
+class CommandPool final {
+ public:
+  CommandPool() {}
+  CommandPool(Device& device, unsigned int queueFamily);
+  CommandPool(VkCommandPool pool) : Pool(pool) {}
 
-	vk::raii::CommandPool &operator*( );
-	Device *GetDevice( );
-	unsigned int GetQueueFamily( );
+  VkCommandPool& operator*() { return Pool; }
+  Device& GetDevice() { return Dev; }
+  unsigned int GetQueueFamily() { return QueueFamily; }
 
-	void Release( );
+  void Release();
 
-	virtual ~CommandPool( ) = default;
-private:
-	vk::raii::CommandPool *Pool = nullptr;
-	unsigned int QueueFamily = 0;
-	Device *Dev = 0;
+  virtual ~CommandPool() = default;
+
+ private:
+  VkCommandPool Pool = nullptr;
+  unsigned int QueueFamily = 0;
+  Device Dev;
 };
 
 #endif
